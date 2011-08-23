00bffb86d2cbc80662d9f40074f83c2e34c4092a
Stephen Paul Weber <singpolyma@singpolyma.net>
2011-08-22 20:24:35 -0500

    Function to get first 10 images

---
+++ lib/paginated_hatom_imgs.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+require 'nokogiri'
+
+$: << File.dirname(__FILE__)
+require 'util'
+require 'find_photo_uri'
+
+# FIXME: slow, maybe parallelize the requests?
+def paginated_hatom_imgs(uri, depth = 0)
+	final_uri, page = fetch(uri)
+	doc = Nokogiri::parse(page.body)
+	imgs = doc.search('.entry-content a').map do |el|
+		next if el.attributes['rel'].to_s =~ /(?:^|\s)(?:tag|bookmark)(?:\s|$)/i
+		next if el.attributes['class'].to_s =~ /(?:^|\s)(?:url|response|username|hashtag)(?:\s|$)/i
+		relative_to_absolute(el.attributes['href'].to_s, final_uri)
+	end.compact.uniq.map do |uri|
+		find_photo_uri(uri)
+	end.compact.uniq
+
+	if imgs.length < 10 && depth < 5
+		if (n = doc.at('*[rel~="next"]'))
+			imgs + paginated_hatom_imgs(relative_to_absolute(
+				n.attributes['href'].to_s, final_uri), depth+1)
+		end
+	end
+
+	imgs
+end
